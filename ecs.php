0105bf020cd2cb8cc32039b9ca48e3b1609995ef
Christian <chrisdaq@gmail.com>
2024-11-28 11:01:28 -0400

    fix quality tool configs

--- ecs.php
+++ ecs.php
@@ -8,10 +8,10 @@ use Symplify\EasyCodingStandard\Config\ECSConfig;
 return ECSConfig::configure()
     ->withPaths([
         __DIR__ . '/app',
-        __DIR__ . '/bootstrap',
-        __DIR__ . '/config',
-        __DIR__ . '/public',
-        __DIR__ . '/resources',
+        // __DIR__ . '/bootstrap',
+        // __DIR__ . '/config',
+        // __DIR__ . '/public',
+        // __DIR__ . '/resources',
         __DIR__ . '/routes',
         __DIR__ . '/tests',
     ])
@@ -29,5 +29,5 @@ return ECSConfig::configure()
         // docblocks: true,
         // comments: true,
     // )
-     
+
      ;
--- rector.php
+++ rector.php
@@ -3,19 +3,30 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
 
 return RectorConfig::configure()
     ->withPaths([
         __DIR__ . '/app',
-        __DIR__ . '/bootstrap',
-        __DIR__ . '/config',
+        // __DIR__ . '/bootstrap',
+        // __DIR__ . '/config',
         // __DIR__ . '/public',
         // __DIR__ . '/resources',
-        __DIR__ . '/routes',
+        // __DIR__ . '/routes',
         __DIR__ . '/tests',
     ])
     // uncomment to reach your current PHP version
-    ->withPhpSets(php83: true)
+    /*->withPhpSets(php83: true)
     ->withTypeCoverageLevel(0)
     ->withDeadCodeLevel(0)
-    ->withCodeQualityLevel(0);
+    ->withCodeQualityLevel(0);*/
+    ->withSets([
+        SetList::DEAD_CODE,
+        LevelSetList::UP_TO_PHP_83,
+        // LaravelSetList::LARAVEL_110,
+    ])
+    ->withRules([
+        AddVoidReturnTypeWhereNoReturnRector::class,
+    ]);
